01d51baa05a302f54b334454c95c2cbdafe7280a
Aleksey <106984793+Lex4509@users.noreply.github.com>
2022-12-06 14:11:41 +0300

    Delete src/main/java/ru/yandex/practicum/filmorate directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/InvalidStatementException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class InvalidStatementException extends RuntimeException{
-
-    public InvalidStatementException(String message) {
-        super(message);
-    }
-}
